57e0a1fb7650646581bb195d34308ba7f08de262
Matt Sears <matt@mattsears.com>
2011-11-27 20:52:25 -0500

    Bump now just moves an item to the top of the stack instead of customizing the position.  Much simpler this way.

--- lib/taco/list.rb
+++ lib/taco/list.rb
@@ -1,25 +1,19 @@
+require 'fileutils'
 
 module Taco
 
-  # The Todo contains many Items. They exist as buckets in which to categorize
+  # A Taco contains many Items. They exist as buckets in which to categorize
   # individual Items. The relationship is maintained in a simple array.
   #
   class List
+    include Taco::Color
 
     # Creates a new Todo instance in-memory.
     #
     # Returns the persisted Todo instance.
     def initialize(options = {})
-      @options, @items = options, []
-      bootstrap
-      load_items
+      @options = options
     end
-
-    # The main todos in the user's home directory
-    FILE = File.expand_path('.taco')
-
-    # Allow to items to be accessible from the outside
-    attr_accessor :items
 
     # Creates a new todo
     #
@@ -29,128 +23,96 @@ module Taco
     # Returns the add todo Item
     def add(todo)
       save do
-        @items.push(Item.new(todo)).last
+        items.push(Item.new(todo)).last
       end
     end
 
     # Removes the todo
     #
     # Example:
-    #   @todo.delete(1)
+    #   @taco.delete(1)
     #
     # Returns the deleted todo Item
     def delete(index)
       save do
-        @items.delete_at(index.to_i-1)
+        items.delete_at(index.to_i-1)
       end
     end
 
     # Marks a todo as done
     #
     # Example:
-    #   @todo.done(1)
+    #   @taco.done(1)
     #
-    # Returns the done todo Item
+    # Returns [Item] the done Item
     def done(index)
       save do
-        @items[index-1].context = '@done'
-        @items[index-1]
+        items[index-1].context = '@done'
+        items[index-1]
       end
     end
 
     # Prints all the active todos in a nice neat format
     #
     # Examples:
-    #   @todo.list @work
+    #   @taco.list @work
     #
     # Returns nothing
     def list(padding = 5)
-      puts "\nTodos:"
+      puts "\nTACOS:"
       puts '-'*(max_length(:text) + max_length(:context) + padding + 3)
-      @items.each_with_index do |todo, index|
-        printf "%s: %-#{max_length(:text)+padding}s%s\n", index+1, todo.text, todo.context
+
+      items.each_with_index do |item, index|
+        if item.context.to_s == @options[:filter] || @options[:filter].nil?
+          printf("[%s] %-#{max_length(:text)+padding}s%s\n",
+            index+1, item.text, colorize(item.context, :cyan))
+        end
       end
     end
 
     # Finds the longest string given the Item attribute
     #
     # Example:
-    #   @todo.max_length(:text)  #=> 29
+    #   @taco.max_length(:text)  #=> 29
     #
-    # Returns Fixnum size
+    # @return [Fixnum] size
     def max_length(attribute)
-      @items.map(&attribute.to_sym).max_by(&:length).size
+      return 0 if items.empty?
+      items.map(&attribute.to_sym).max_by(&:length).size
     end
 
     # Moves a todo up or down in priority
     #
     # Example:
-    #   @todo.bump(2, +1)
+    #   @taco.bump(2)
     #
-    def bump(index, position = 1)
+    # @return [Item]
+    def bump(index)
       save do
-        index, position = index.to_i-1, position.to_i-1
-        @items.insert(position, @items.delete_at(index))
-        @items[position]
+        items.insert(0, items.delete_at(index.to_i-1))
+        items.first
       end
-    end
-
-    # Accessor for the todo list file
-    #
-    # Returns String file path
-    def file
-      @file ||= File.exist?(FILE) ? FILE : "#{ENV['HOME']}/.taco"
-    end
-
-    # Formats the current set of todos
-    #
-    # Returns a lovely hash
-    def to_hash
-      @items.group_by(&:context).inject({}) do |h,(k,v)|
-        h[k.to_sym] = v.map(&:text); h
-      end
-    end
-
-    # Loads the yaml todos file and creates a hash
-    #
-    # Returns the items loaded from the file
-    def load_items
-      YAML.load_file(file).each do |key, texts|
-        texts.each do |text|
-          if key.to_s == @options[:filter] || @options[:filter].nil?
-            @items << Item.new("#{text} #{key}") if key.to_s != '@done'
-          end
-        end
-      end
-      @items
     end
 
     # Implodes all the todo items save an empty file
     #
     # Returns nothing
     def clear!
-      @items.clear
+      items.clear
       save
     end
-
-    private
 
     # Saves the current list of todos to disk
     #
     # Returns nothing
     def save(&block)
       returning = yield if block_given?
-      File.open(file, "w") {|f| f.write(to_hash.to_yaml) }
+      Taco.storage.save
       returning
     end
 
-    # Creates a new todo file if none is present
-    #
-    # Returns nothing
-    def bootstrap
-      return if File.exist?(file)
-      @items = [Taco::Item.new('Add an item! @empty')]
-      save
+    def items
+      Taco.storage.items
     end
 
   end
--- test/list_test.rb
+++ test/list_test.rb
@@ -4,6 +4,7 @@ require 'helper'
 describe Taco::List do
 
   before do
+    Taco::List.any_instance.stubs(:file).returns("test/examples/tacos")
     @list = Taco::List.new
     @list.clear!
     @list.add 'Take the dog for a walk'
@@ -12,16 +13,16 @@ describe Taco::List do
     @list.add 'Buy Ruby Nerd from RubyRags @home'
   end
 
-  # it "finds the file path of the todo list" do
-  #   @list.file.must_equal File.expand_path('.taco')
-  # end
+  it "finds the file path of the todo list" do
+    @list.file.must_equal 'test/examples/tacos'
+  end
 
   it "adds the todo to the stack" do
     @list.items.size.must_equal 4
   end
 
   it "creates a hash of attributes from the todo items" do
-    @list.to_hash.must_equal({
+    Taco.storage.to_hash.must_equal({
         :@next => ["Take the dog for a walk"],
         :@work => ["Pay lease bill"],
         :@home => ["Buy Duck Typing from RubyRags", "Buy Ruby Nerd from RubyRags"]
@@ -35,6 +36,12 @@ describe Taco::List do
 
   it 'completes a todo' do
     @list.done(2).context.must_equal "@done"
+  end
+
+
+  it 'should move the specific item to the top' do
+    @list.bump(3)
+    @list.items.first.text.must_equal 'Buy Duck Typing from RubyRags'
   end
 
 end
