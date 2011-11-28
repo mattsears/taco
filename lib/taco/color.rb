91c664bbc1cfd3882da693c0d2a7acc9a1dabb58
Matt Sears <matt@mattsears.com>
2011-11-27 20:50:14 -0500

    Add color module to help with colorizing the display.

---
+++ lib/taco/color.rb
@@ -0,0 +1,47 @@
+module Taco
+
+  # Color collects some methods for colorizing terminal output.
+  module Color
+    extend self
+
+    CODES = {
+      :reset   => "\e[0m",
+      :cyan    => "\e[36m",
+      :magenta => "\e[35m",
+      :red     => "\e[31m",
+      :gray    => "\e[30m",
+      :yellow  => "\e[33m"
+    }
+
+    # Tries to enable Windows support if on that platform.
+    #
+    # Returns nothing.
+    def self.included(other)
+      if RUBY_PLATFORM =~ /win32/ || RUBY_PLATFORM =~ /mingw32/
+        require 'Win32/Console/ANSI'
+      end
+    rescue LoadError
+      # Oh well, we tried.
+    end
+
+    # Wraps the given string in ANSI color codes
+    #
+    # string     - The String to wrap.
+    # color_code - The String representing he ANSI color code
+    #
+    # Examples
+    #
+    #   colorize("Taco town!", :magenta)
+    #   # => "\e[35mTaco town!\e[0m"
+    #
+    # Returns the wrapped String.
+    def colorize(string, color_code)
+      "#{CODES[color_code] || color_code}#{string}#{CODES[:reset]}"
+    end
+
+    # Set up shortcut methods to all the codes define in CODES.
+    self.class_eval(CODES.keys.reject {|color| color == :reset }.map do |color|
+        "def #{color}(string); colorize(string, :#{color}); end"
+      end.join("\n"))
+  end
+end
